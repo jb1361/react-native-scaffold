ad31f28c278add4fa74a3991dc670a3c7ff5dcd5
jb1361 <justinbutler4@hotmail.com>
2018-11-17 18:19:32 -0500

    rename app to src

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 import { Client } from 'bugsnag-react-native';
 const bugsnag = new Client("3d3f6ed00f43fa9815379c72e47384c4");
