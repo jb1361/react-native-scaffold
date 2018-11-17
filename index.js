321551e124380ec6f6231f31b65da7584c93e945
jb1361 <justinbutler4@hotmail.com>
2018-11-17 17:08:08 -0500

    Add bugsnag

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+import { Client } from 'bugsnag-react-native';
+const bugsnag = new Client("3d3f6ed00f43fa9815379c72e47384c4");
 AppRegistry.registerComponent(appName, () => App);
