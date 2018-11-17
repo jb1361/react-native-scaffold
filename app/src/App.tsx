d79c92ec3023b712e782198228d9a57103b10c45
jb1361 <justinbutler4@hotmail.com>
2018-11-17 18:57:20 -0500

    edit tsconfig

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,50 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
-import React from 'react'
-import { Component } from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isShowingText: true };
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+        // Toggle the state every second
+        setInterval(() => (
+            this.setState(previousState => (
+                { isShowingText: !previousState.isShowingText }
+            ))
+        ), 1000);
+    }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    render() {
+        if (!this.state.isShowingText) {
+            return null;
+        }
+
+        return (
+            <Text>{this.props.text}</Text>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class BlinkApp extends Component {
+    render() {
+        return (
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
+            </View>
+        );
+    }
+}
