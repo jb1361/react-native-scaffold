73bb221f830afb4feb086719962798a21fb3749d
jb1361 <justinbutler4@hotmail.com>
2018-11-17 19:00:18 -0500

    remove hello.tsx and revert app.tsx to default

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,39 +1,52 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
 
-class Blink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { isShowingText: true };
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-        // Toggle the state every second
-        setInterval(() => (
-            this.setState(previousState => (
-                { isShowingText: !previousState.isShowingText }
-            ))
-        ), 1000);
-    }
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
 
+interface Props {}
+export default class App extends Component<Props> {
     render() {
-        if (!this.state.isShowingText) {
-            return null;
-        }
-
+        throw new Error('This is an error to test Sentry');
         return (
-            <Text>{this.props.text}</Text>
-        );
-    }
-}
-
-export default class BlinkApp extends Component {
-    render() {
-        return (
-            <View>
-                <Blink text='I love to blink' />
-                <Blink text='Yes blinking is so great' />
-                <Blink text='Why did they ever take this out of HTML' />
-                <Blink text='Look at me look at me look at me' />
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!!!</Text>
+                <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
